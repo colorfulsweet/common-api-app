53e86615882b2df16d9f3c7dfd358f0ee9236bcc
结发受长生 <init_pro@aliyun.com>
2019-11-14 00:21:37 +0800

    引入flutter_staggered_grid_view

--- lib/pages/child/photo_wall.dart
+++ lib/pages/child/photo_wall.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import 'package:blog_api/common/profile_notifier.dart';
+import 'package:blog_api/common/global.dart';
 
 /// 照片墙
 class PhotoWall extends StatefulWidget {
@@ -14,6 +17,8 @@ class PhotoWall extends StatefulWidget {
 
 class _PhotoWallState extends State<PhotoWall> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<double> heights = [50,80,30,60,55,90];
+  List<String> imgUrls = [];
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +35,37 @@ class _PhotoWallState extends State<PhotoWall> {
         ),
         ],
       ),
-      body: Container()
+      body:  StaggeredGridView.countBuilder(
+//        controller: _scrollController,
+        itemCount: this.imgUrls.length,
+        primary: false,
+        crossAxisCount: 4,
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+        padding: EdgeInsets.all(10),
+        itemBuilder: (context, index) => Container(
+          child: Image.network(
+            this.imgUrls[index], // 图片地址
+            fit: BoxFit.fitWidth, // fit属性指定控制图片拉伸适应容器的方式, 这里是按高度适应
+          ),
+        ),
+        staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+      ),
     );
   }
+  @override
+  void initState() {
+    this._loadImagesData();
+    super.initState();
+  }
 
+  void _loadImagesData () async {
+    Response response = await Dio().get('${Global.API_BASE_PATH}common/photos?start=0&limit=20');
+    print(response.data['data']);
+//    var thumbnailUrls = response.data['data'].map((item) {
+//      return 'https://cdn.colorfulsweet.site/' + item['thumbnail'];
+//    });
+//    print(thumbnailUrls);
+//    imgUrls.addAll(thumbnailUrls);
+  }
 }
